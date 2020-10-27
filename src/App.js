75ab428186092af28c83ddb2b189b93c351fb51e
Joe Martella <joem@microsoft.com>
2020-10-27 11:45:08 -0700

    ting

--- src/App.js
+++ src/App.js
@@ -7,27 +7,34 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <strong>old:</strong>
+      <div className="box">
+        <strong>contact email you used:</strong>
         <input
-          style={{ marginLeft: "12px", marginBottom: "12px" }}
           value={oldAccount}
           onChange={(e) => setOldAccount(e.target.value)}
         />
+        <p className="note">
+          This is the email that received the shortfuts premium receipt.
+        </p>
       </div>
 
-      <div>
-        <strong>new:</strong>
+      <div className="box">
+        <strong>your shortfuts account:</strong>
         <input
-          style={{ marginLeft: "12px", marginBottom: "12px" }}
           value={newAccount}
           onChange={(e) => setNewAccount(e.target.value)}
         />
+        <p className="note">
+          You can find this email on your shortfuts app (where you were supposed
+          to find it before you purchased).
+        </p>
       </div>
       <button
+        style={{ marginTop: "6px" }}
         onClick={() => {
           fetch(
             `https://shortfuts-server.herokuapp.com/update-premium-user/${oldAccount}/${newAccount}`,
+            // `http://localhost:3000/update-premium-user/${oldAccount}/${newAccount}`,
             {
               method: "POST",
               headers: {
