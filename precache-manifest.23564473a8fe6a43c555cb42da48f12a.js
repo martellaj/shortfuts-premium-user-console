5db419469e44f128827842d2ad3f3a168fb85e35
Joe Martella <joem@microsoft.com>
2020-10-27 11:50:39 -0700

    Updates

---
+++ precache-manifest.23564473a8fe6a43c555cb42da48f12a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49dc7fe9e8f34520a1b571f3e1c7a87c",
+    "url": "/shortfuts-premium-user-console/index.html"
+  },
+  {
+    "revision": "198995fd4c91def97331",
+    "url": "/shortfuts-premium-user-console/static/css/main.dbc58f77.chunk.css"
+  },
+  {
+    "revision": "5f7e7315470608e569ea",
+    "url": "/shortfuts-premium-user-console/static/js/2.44d11590.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/shortfuts-premium-user-console/static/js/2.44d11590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "198995fd4c91def97331",
+    "url": "/shortfuts-premium-user-console/static/js/main.369a5785.chunk.js"
+  },
+  {
+    "revision": "7fa9044f1513fac4803d",
+    "url": "/shortfuts-premium-user-console/static/js/runtime-main.bab0789b.js"
+  }
+]);
